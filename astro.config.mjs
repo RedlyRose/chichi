da1c920a4cffb8fc8d53a90d808a67fb8f36a42d
purplehallos <hallospurpleyard@gmail.com>
2026-03-16 23:54:02 +0100

    fix: set imageService to passthrough for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,9 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'passthrough'
+  }),
   integrations: [react()],
 
   vite: {
